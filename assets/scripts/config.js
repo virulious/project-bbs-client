2db57d552becc64b53e1ba5b0443e1b84626fff4
Robert Cronin <Virulious@hotmail.com>
2020-05-18 13:33:25 -0400

    Change apiUrl for production
    Change production to https://git.heroku.com/young-eyrie-49492.git

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://young-eyrie-49492.herokuapp.com/',
+  production: 'https://git.heroku.com/young-eyrie-49492.git',
   development: 'http://localhost:4741'
 }
 
